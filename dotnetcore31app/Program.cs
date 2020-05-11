c49738a9754d2f228ffc3d63ad32a1f526897571
Jeff Goergen <jeffg@wambatech.com>
2020-05-11 14:32:49 -0400

    small tweaks

--- dotnetcore31app/Program.cs
+++ dotnetcore31app/Program.cs
@@ -14,6 +14,8 @@ namespace HubspotMailingListExportCombiner
     {
         private static bool DEBUG = false;
         private static List<string> KEEP_EVENT_TYPES = new List<string>() { EventType.DELIVERED.Value, EventType.OPEN.Value, EventType.CLICK.Value, EventType.BOUNCE.Value, EventType.UNSUBSCRIBE.Value };
+        private static List<List<HubspotMailingListExportEntry>> lists;
+        private static List<CombinedDataEntry> combinedList;
 
         static void Main(string[] args)
         {
@@ -51,7 +53,7 @@ namespace HubspotMailingListExportCombiner
             }
 
             Console.WriteLine("Reading data from files", Color.Yellow);
-            var lists = new List<List<HubspotMailingListExportEntry>>();
+            lists = new List<List<HubspotMailingListExportEntry>>();
             var index = 0;
 
             foreach (var path in files)
@@ -64,7 +66,78 @@ namespace HubspotMailingListExportCombiner
             }
 
             Console.WriteLine("Combining data", Color.Yellow);
-            var combinedList = new List<CombinedDataEntry>();
+            combineLists();
+
+            Console.WriteLine($"Generating output.csv file with {combinedList.Count} entries.", Color.Yellow);
+            using (var writer = new StreamWriter("output.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(combinedList);
+            }
+
+            Console.WriteLine("Done, press enter to close", Color.Green);
+            Console.ReadLine();
+        }
+
+        static CombinedDataEntry BuildCombinedDataEntry(HubspotMailingListExportEntry entry)
+        {
+            var domainParts = entry.Recipient.Split("@")[1].Split(".");
+            var urlParts = entry.ClickURL.ToLower().Replace("http://", "").Replace("https://", "").Split("/");
+            var urlType =
+                urlParts.Count() < 2 ?
+                    "" :
+                    urlParts[1].Contains("?") ?
+                        "" :
+                        urlParts[1];
+            return
+                new CombinedDataEntry()
+                {
+                    BounceMessage = entry.BounceMessage,
+                    BounceReason = entry.BounceReason,
+                    BounceStatus = entry.BounceStatus,
+                    UrlType = urlType,
+                    ClickURL = entry.ClickURL,
+                    Company = String.Join(".", domainParts.Take(domainParts.Length - 1)),
+                    EventType = entry.EventType,
+                    OpenDurationSeconds = CleanOpenDurationTime(entry.OpenDuration),
+                    Recipient = entry.Recipient,
+                    EventCreatedDate = entry.EventCreatedDate.Split(" ")[0],
+                    EventCreatedTime = entry.EventCreatedDate.Split(" ")[1]
+                };
+        }
+
+        static string CleanOpenDurationTime(string time)
+        {
+            if (string.IsNullOrWhiteSpace(time) || time == "null" || time == "0")
+            {
+                return "";
+            }
+            else
+            {
+                try
+                {
+                    var timeInMS = int.Parse(time);
+                    return (timeInMS / 1000).ToString();
+                }
+                catch
+                {
+                    return "";
+                }
+            }
+        }
+
+        static List<HubspotMailingListExportEntry> GetCSVData(string path)
+        {
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<HubspotMailingListExportEntry>().ToList();
+            }
+        }
+    
+        static void combineLists()
+        {
+            combinedList = new List<CombinedDataEntry>();
             var listIndex = 0;
             foreach (var listData in lists)
             {
@@ -136,72 +209,6 @@ namespace HubspotMailingListExportCombiner
                 }
                 Console.WriteLine($" Done.", Color.Yellow);
                 listIndex++;
-            }
-
-            Console.WriteLine($"Generating output.csv file with {combinedList.Count} entries.", Color.Yellow);
-            using (var writer = new StreamWriter("output.csv"))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            {
-                csv.WriteRecords(combinedList);
-            }
-
-            Console.WriteLine("Done, press enter to close", Color.Green);
-            Console.ReadLine();
-        }
-
-        static CombinedDataEntry BuildCombinedDataEntry(HubspotMailingListExportEntry entry)
-        {
-            var domainParts = entry.Recipient.Split("@")[1].Split(".");
-            var urlParts = entry.ClickURL.ToLower().Replace("http://", "").Replace("https://", "").Split("/");
-            var urlType =
-                urlParts.Count() < 2 ?
-                    "" :
-                    urlParts[1].Contains("?") ?
-                        "" :
-                        urlParts[1];
-            return
-                new CombinedDataEntry()
-                {
-                    BounceMessage = entry.BounceMessage,
-                    BounceReason = entry.BounceReason,
-                    BounceStatus = entry.BounceStatus,
-                    UrlType = urlType,
-                    ClickURL = entry.ClickURL,
-                    Company = String.Join(".", domainParts.Take(domainParts.Length - 1)),
-                    EventType = entry.EventType,
-                    OpenDurationSeconds = CleanOpenDurationTime(entry.OpenDuration),
-                    Recipient = entry.Recipient,
-                    EventCreatedDate = entry.EventCreatedDate.Split(" ")[0],
-                    EventCreatedTime = entry.EventCreatedDate.Split(" ")[1]
-                };
-        }
-
-        static string CleanOpenDurationTime(string time)
-        {
-            if (string.IsNullOrWhiteSpace(time) || time == "null" || time == "0")
-            {
-                return "";
-            }
-            else
-            {
-                try
-                {
-                    var timeInMS = int.Parse(time);
-                    return (timeInMS / 1000).ToString();
-                }
-                catch
-                {
-                    return "";
-                }
-            }
-        }
-
-        static List<HubspotMailingListExportEntry> GetCSVData(string path)
-        {
-            using (var reader = new StreamReader(path))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                return csv.GetRecords<HubspotMailingListExportEntry>().ToList();
             }
         }
     }
